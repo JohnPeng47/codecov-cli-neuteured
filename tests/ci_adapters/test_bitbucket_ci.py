3711d27ba5196a697e16a16a790e950688a44de2
JohnPeng47 <johnpeng47@gmail.com>
2024-06-30 15:09:39 +0000

    COWBOY generated tests/ci_adapters/test_bitbucket_ci.py: 2

--- tests/ci_adapters/test_bitbucket_ci.py
+++ tests/ci_adapters/test_bitbucket_ci.py
@@ -18,12 +18,6 @@ class BitbucketEnvEnum(str, Enum):
 
 class TestBitbucket(object):
 
-
-
-
-
-
-
     @pytest.mark.parametrize(
         "env_dict,expected",
         [
@@ -42,3 +36,9 @@ class TestBitbucket(object):
             BitbucketAdapter().get_fallback_value(FallbackFieldEnum.service)
             == "bitbucket"
         )
+
+    def test_get_build_url(self):
+        assert BitbucketAdapter()._get_build_url() is None
+
+    def test_get_git_service(self):
+        assert BitbucketAdapter()._get_git_service() is None
